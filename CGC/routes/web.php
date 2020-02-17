2edbfdc5e15cd2b5acc10849a2629c97cefedf14
wilfried <wilfriedmambou300>
2020-02-18 00:33:57 +0100

    creation des routes question/answer/comments

--- CGC/routes/web.php
+++ CGC/routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::resource('questions','QuestionsController');
+Route::resource('answers','AnswerController',['except'=>['index','create','show']]);
+Route::resource('comments','CommentsController');
+
